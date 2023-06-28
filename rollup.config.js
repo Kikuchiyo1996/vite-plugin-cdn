5ede6c09b5764901611703c646e4d255fb4a8381
王丹丹 <630137139@qq.com>
2023-06-28 18:39:21 +0800

    feat: add jsOptions

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from 'rollup-plugin-typescript2'
+/* import nodeResolve from '@rollup/plugin-node-resolve'
+import builtins from 'rollup-plugin-node-builtins'
+import cjs from '@rollup/plugin-commonjs' */
+
+export default [
+	{
+		input: `src/index.ts`,
+		plugins: [typescript()],
+		output: [
+			{
+				dir: `dist/`,
+				format: 'cjs',
+				entryFileNames: '[name].cjs.js',
+			},
+			{
+				dir: `dist/`,
+				format: 'umd',
+				name: 'vite-plugin-cdn',
+				inlineDynamicImports: true,
+				entryFileNames: '[name].umd.js',
+			},
+			{
+				dir: `dist/`,
+				format: 'es',
+				entryFileNames: '[name].esm.js',
+			},
+		],
+	},
+]
